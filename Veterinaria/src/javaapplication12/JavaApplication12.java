7f3aec79eed19fab9025e6e86123fd2e1de70d22
Guadalupe1995 <a1410373@iberotec.edu.pe>
2015-06-13 10:43:30 -0500

    hice un comentario
    buen Dia

--- Veterinaria/src/javaapplication12/JavaApplication12.java
+++ Veterinaria/src/javaapplication12/JavaApplication12.java
@@ -16,6 +16,6 @@ public class JavaApplication12 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+    }//tenemos k avanzar el codigo
     
 }
